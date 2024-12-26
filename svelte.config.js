874ecf28434b45d8db146778b34c493a0efebd00
Albert Johannessen <albertjohannessen@live.no>
2024-12-26 03:16:25 +0100

    m

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+//import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
+    //preprocess: vitePreprocess(),
 
     kit: {
         adapter: adapter({
-            pages: "build",
-            assets: "build",
-            fallback: undefined,
-            precompress: false,
-            strict: true
+            //pages: "build",
+            //assets: "build",
+            fallback: "404.html",
+            //precompress: false,
+            //strict: true
         }),
         paths: {
             base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
