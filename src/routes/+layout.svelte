1134d2e6dbc7a58f7984c6a1c7de59db1c5742da
Albert Johannessen <albertjohannessen@live.no>
2024-12-26 01:07:37 +0100

    SvelteKit and Skeleton setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import "../app.css";
+    let { children } = $props();
+</script>
+
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+</ul>
+
+{@render children()}
