941964aeeca3940f155ab3e9311391df3fb12b86
Albert Johannessen <albertjohannessen@live.no>
2024-12-27 19:34:46 +0100

    Maybe fixed relative links - take 2

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,26 +3,26 @@
     import { base } from "$app/paths";
 
     const links = [
-        { url: "/", label: "Home" },
-        { url: "/about", label: "About" },
-        { url: "/publications", label: "Publications" },
-        { url: "/interests", label: "Interests" },
-        { url: "/tech", label: "Tech Stack" }
+        { url: base + "/", label: "Home" },
+        { url: base + "/about", label: "About" },
+        { url: base + "/publications", label: "Publications" },
+        { url: base + "/interests", label: "Interests" },
+        { url: base + "/tech", label: "Tech Stack" }
     ];
 </script>
 
 <header class="flex w-full flex-col">
     <section class="mx-auto flex w-4/5 justify-center">
         <div class="p-4">
-            <a href="/"><img src="images/Hex.png" width="75" height="75" alt="Logo" /></a>
+            <a href="{base}/"><img src="images/Hex.png" width="75" height="75" alt="Logo" /></a>
         </div>
         <div class="grow content-center text-left">
-            <a href="/"><h3 class="h3">Albert Johannessen</h3></a>
+            <a href="{base}/"><h3 class="h3">Albert Johannessen</h3></a>
         </div>
-        <!-- <nav class="content-center gap-2"> -->
-        <!--     {#each links as link} -->
-        <!--         <a href={base}{link.url} class="btn-lg">{link.label}</a> -->
-        <!--     {/each} -->
-        <!-- </nav> -->
+        <nav class="content-center gap-2">
+            {#each links as link}
+                <a href={link.url} class="btn-lg">{link.label}</a>
+            {/each}
+        </nav>
     </section>
 </header>
