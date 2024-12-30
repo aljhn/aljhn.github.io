8abd9c8f580b34d936d4d428785558662bf00741
Albert Johannessen <albertjohannessen@live.no>
2024-12-30 19:12:34 +0100

    wormification

--- src/lib/Lorenz.svelte
+++ src/lib/Lorenz.svelte
@@ -5,102 +5,155 @@
         return ((n % m) + m) % m;
     }
 
+    function getRandom(low: number, high: number) {
+        return Math.random() * (high - low) + low;
+    }
+
+    class Point {
+        x: number;
+        y: number;
+        z: number;
+
+        constructor(x: number, y: number, z: number) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    class Particle {
+        path: Point[];
+        pathLength: number;
+        currentPointIndex: number = 0;
+
+        constructor(initialPoint: Point, pathLength: number) {
+            this.path = [initialPoint];
+            this.pathLength = pathLength;
+        }
+
+        update(
+            ctx: CanvasRenderingContext2D,
+            centerX: number,
+            centerY: number,
+            scale: number,
+            speedScale: number,
+            dt: number,
+            sigma: number,
+            rho: number,
+            beta: number
+        ) {
+            const x: number = this.path[this.currentPointIndex].x;
+            const y: number = this.path[this.currentPointIndex].y;
+            const z: number = this.path[this.currentPointIndex].z;
+
+            const dx: number = sigma * (y - x);
+            const dy: number = x * (rho - z) - y;
+            const dz: number = x * y - beta * z;
+
+            const nextX: number = x + dx * dt * speedScale;
+            const nextY: number = y + dy * dt * speedScale;
+            const nextZ: number = z + dz * dt * speedScale;
+
+            if (this.path.length < this.pathLength) {
+                this.path.push(new Point(nextX, nextY, nextZ));
+                this.currentPointIndex++;
+            } else {
+                this.currentPointIndex = mod(this.currentPointIndex + 1, this.pathLength);
+                this.path[this.currentPointIndex].x = nextX;
+                this.path[this.currentPointIndex].y = nextY;
+                this.path[this.currentPointIndex].z = nextZ;
+            }
+
+            const deltaX: number =
+                this.path[this.currentPointIndex].x - this.path[mod(this.currentPointIndex - 1, this.path.length)].x;
+            const deltaZ: number =
+                this.path[this.currentPointIndex].z - this.path[mod(this.currentPointIndex - 1, this.path.length)].z;
+            const angle: number = (Math.atan2(deltaZ, deltaX) * 180) / Math.PI + 180;
+            ctx.fillStyle = "hsl(" + angle + ", 40%, 50%)";
+            // ctx.fillStyle = "#ffffff";
+            ctx.beginPath();
+            ctx.arc(
+                centerX + this.path[this.currentPointIndex].x * scale,
+                centerY - this.path[this.currentPointIndex].z * scale,
+                3,
+                0,
+                2 * Math.PI
+            );
+            ctx.fill();
+
+            for (let i = 0; i < this.path.length - 1; i++) {
+                const index1: number = mod(this.currentPointIndex + 1 + i, this.path.length);
+                const index2: number = mod(this.currentPointIndex + 2 + i, this.path.length);
+
+                const alphaCounter: number = mod(index1 - this.currentPointIndex, this.path.length);
+                // const alphaValue: number = Math.floor(Math.pow(alphaCounter / this.path.length, 2) * 255);
+                // let alpha: string = alphaValue.toString(16);
+                // if (alpha.length < 2) {
+                //     alpha = "0" + alpha;
+                // }
+                // ctx.strokeStyle = "#ffffff" + alpha;
+
+                const alphaValue: number = Math.pow(alphaCounter / this.path.length, 2);
+                const angle: number =
+                    (Math.atan2(this.path[index2].z - this.path[index1].z, this.path[index2].x - this.path[index1].x) *
+                        180) /
+                        Math.PI +
+                    180;
+                ctx.strokeStyle = "hsla(" + angle + ", 40%, 50%," + alphaValue + ")";
+                ctx.lineWidth = 3;
+
+                ctx.beginPath();
+                ctx.moveTo(centerX + this.path[index1].x * scale, centerY - this.path[index1].z * scale);
+                ctx.lineTo(centerX + this.path[index2].x * scale, centerY - this.path[index2].z * scale);
+                ctx.stroke();
+            }
+        }
+    }
+
     $effect(() => {
         const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
         if (ctx != null) {
             ctx.fillStyle = "#222222";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-            let x: number = 0.0;
-            let y: number = 1.0;
-            let z: number = 1.05;
-
-            x += Math.random() * 2 - 1;
-            y += Math.random() * 2 - 1;
-            z += Math.random() * 2 - 1;
+            let x0: number = 0.0;
+            let y0: number = 1.0;
+            let z0: number = 1.05;
 
             const rho: number = 28;
             const sigma: number = 10;
             const beta: number = 8 / 3;
 
-            const FPS: number = 60;
-            const dt: number = 1 / FPS;
-            const interval: number = 1000 * dt;
+            const maxPoints: number = 50;
+            const particleAmount = 50;
+            let particles: Particle[] = [];
+            for (let i = 0; i < particleAmount; i++) {
+                const initialPoint: Point = new Point(
+                    x0 + getRandom(-1, 1),
+                    y0 + getRandom(-1, 1),
+                    z0 + getRandom(-1, 1)
+                );
+                particles.push(new Particle(initialPoint, maxPoints));
+            }
 
-            const maxPoints: number = 100;
-            let pathX: number[] = new Array(maxPoints);
-            let pathZ: number[] = new Array(maxPoints);
-
-            let currentPointIndex: number = 0;
-            let pointFirstIndex: number = 0;
-
-            let addNewPointCounter: number = 0;
-            const addNewPointInterval: number = 2;
+            const fps: number = 60;
+            const dt: number = 1 / fps;
+            const drawInterval: number = 1000 * dt;
 
             const scale: number = 10;
             const centerX: number = canvas.width / 2;
             const centerY: number = canvas.height / 2 + 30 * scale;
 
+            const speedScale: number = 0.5;
+
             setInterval(() => {
                 ctx.fillStyle = "#222222";
                 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-                if (addNewPointCounter % addNewPointInterval == 0) {
-                    pathX[currentPointIndex] = x;
-                    pathZ[currentPointIndex] = z;
-                    currentPointIndex++;
-                    if (currentPointIndex >= maxPoints) {
-                        currentPointIndex = 0;
-                    }
-                    if (currentPointIndex <= pointFirstIndex) {
-                        pointFirstIndex++;
-                        if (pointFirstIndex >= maxPoints) {
-                            pointFirstIndex = 0;
-                        }
-                    }
+                for (let i = 0; i < particles.length; i++) {
+                    particles[i].update(ctx, centerX, centerY, scale, speedScale, dt, sigma, rho, beta);
                 }
-                addNewPointCounter++;
-
-                let pointIndex: number = pointFirstIndex;
-                while (pointIndex != currentPointIndex) {
-                    //const alphaCounter = (((pointIndex - currentPointIndex) % maxPoints) + maxPoints) % maxPoints;
-                    const alphaCounter: number = mod(pointIndex - currentPointIndex, maxPoints);
-                    const alphaValue: number = Math.floor(Math.pow(alphaCounter / maxPoints, 4) * 255);
-                    let alpha: string = alphaValue.toString(16);
-                    if (alpha.length < 2) {
-                        alpha = "0" + alpha;
-                    }
-
-                    ctx.strokeStyle = "#ffffff" + alpha;
-                    ctx.lineWidth = 3;
-                    ctx.beginPath();
-                    ctx.moveTo(centerX + pathX[pointIndex] * scale, centerY - pathZ[pointIndex] * scale);
-                    if (pointIndex > 0) {
-                        ctx.lineTo(centerX + pathX[pointIndex - 1] * scale, centerY - pathZ[pointIndex - 1] * scale);
-                    } else {
-                        ctx.lineTo(centerX + pathX[maxPoints - 1] * scale, centerY - pathZ[maxPoints - 1] * scale);
-                    }
-                    ctx.stroke();
-
-                    pointIndex++;
-                    if (pointIndex >= maxPoints) {
-                        pointIndex = 0;
-                    }
-                }
-
-                ctx.fillStyle = "#ffffff";
-                ctx.beginPath();
-                ctx.arc(centerX + x * scale, centerY - z * scale, 5, 0, 2 * Math.PI);
-                ctx.fill();
-
-                const dx: number = sigma * (y - x);
-                const dy: number = x * (rho - z) - y;
-                const dz: number = x * y - beta * z;
-
-                x += dx * dt * 0.5;
-                y += dy * dt * 0.5;
-                z += dz * dt * 0.5;
-            }, interval);
+            }, drawInterval);
         }
     });
 </script>
