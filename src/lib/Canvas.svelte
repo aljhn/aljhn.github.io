5992a8ae654227d249a756a228ba420e33574644
Albert Johannessen <albertjohannessen@live.no>
2025-01-07 19:48:02 +0100

    reverted back to explicit euler integration. Removed circles at the end of the trails

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -37,6 +37,15 @@
         }
     }
 
+    class Euler extends ButcherTable {
+        constructor() {
+            const a: number[][] = [[0]];
+            const b: number[] = [1];
+            const c: number[] = [0];
+            super(a, b, c);
+        }
+    }
+
     class RK4 extends ButcherTable {
         constructor() {
             const a: number[][] = [
@@ -210,7 +219,8 @@
             const p: Point = this.path[this.currentPointIndex];
 
             const h: number = dt * speedScale;
-            const p_next = simulator.next(0, p, h);
+            // const p_next: Point = simulator.next(0, p, h);
+            const p_next: Point = addPoints(p, mulPoint(simulator.system(0, p, simulator.args), h));
 
             if (this.path.length < this.pathLength) {
                 this.path.push(p_next);
@@ -220,21 +230,21 @@
                 this.path[this.currentPointIndex] = p_next; // TODO: insert everything inplace instead of so many allocations
             }
 
-            const index1: number = mod(this.currentPointIndex - 1, this.path.length);
-            const index2: number = this.currentPointIndex;
-            const deltaX: number = this.path[index2].x - this.path[index1].x;
-            const deltaZ: number = this.path[index2].z - this.path[index1].z;
-            const angle: number = (Math.atan2(deltaZ, deltaX) * 180) / Math.PI + 180;
-            ctx.fillStyle = "hsl(" + angle + ", 40%, 50%)";
-            ctx.beginPath();
-            ctx.arc(
-                centerX + this.path[this.currentPointIndex].x * scaleX,
-                centerY - this.path[this.currentPointIndex].z * scaleY,
-                3,
-                0,
-                2 * Math.PI
-            );
-            ctx.fill();
+            // const index1: number = mod(this.currentPointIndex - 1, this.path.length);
+            // const index2: number = this.currentPointIndex;
+            // const deltaX: number = this.path[index2].x - this.path[index1].x;
+            // const deltaZ: number = this.path[index2].z - this.path[index1].z;
+            // const angle: number = (Math.atan2(deltaZ, deltaX) * 180) / Math.PI + 180;
+            // ctx.fillStyle = "hsl(" + angle + ", 40%, 50%)";
+            // ctx.beginPath();
+            // ctx.arc(
+            //     centerX + this.path[this.currentPointIndex].x * scaleX,
+            //     centerY - this.path[this.currentPointIndex].z * scaleY,
+            //     3,
+            //     0,
+            //     2 * Math.PI
+            // );
+            // ctx.fill();
 
             for (let i = 0; i < this.path.length - 1; i++) {
                 const index1: number = mod(this.currentPointIndex + 1 + i, this.path.length);
@@ -273,14 +283,15 @@
             const system: Function = lorenz;
 
             // const table: ButcherTable = new RK4();
-            // const simulator: Integrator = new Integrator(table, system, args);
-            const table: ButcherTable = new Dopri5();
-            const atol: number = 1e-9;
-            const rtol: number = 1e-3;
-            const simulator: AdaptiveIntegrator = new AdaptiveIntegrator(table, system, args, atol, rtol);
+            const table: ButcherTable = new Euler();
+            const simulator: Integrator = new Integrator(table, system, args);
+            // const table: ButcherTable = new Dopri5();
+            // const atol: number = 1e-8;
+            // const rtol: number = 1e-3;
+            // const simulator: AdaptiveIntegrator = new AdaptiveIntegrator(table, system, args, atol, rtol);
 
-            const boundingBoxX0: number = -50;
-            const boundingBoxX1: number = 50;
+            const boundingBoxX0: number = -40;
+            const boundingBoxX1: number = 40;
             const boundingBoxY0: number = -10;
             const boundingBoxY1: number = 60;
 
@@ -297,7 +308,7 @@
                 particles.push(new Particle(initialPoint, maxPoints));
             }
 
-            const speedScale: number = 0.1;
+            const speedScale: number = 0.3;
 
             const canvasDiv = document.getElementById("canvasDiv");
 
