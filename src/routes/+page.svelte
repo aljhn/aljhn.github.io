b087d46f217517c7959f926b6b05fc90af8e6d0f
Albert Johannessen <albertjohannessen@live.no>
2026-06-06 16:40:36 +0200

    separated out particle simulator into more files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import Meta from "$lib/Meta.svelte";
-    import Canvas from "$lib/Canvas.svelte";
+    import ParticleCanvas from "$lib/Particles/Canvas.svelte";
 </script>
 
 <Meta name="Home" />
 
-<Canvas />
+<ParticleCanvas />
