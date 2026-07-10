fbafbd4a4f3524f1e1ce8d0f0a5de0dcb9f5449a
Albert Johannessen <albertjohannessen@live.no>
2026-07-10 15:57:57 +0200

    found a way to make enhanced:img work with mdsvex

---
+++ src/lib/images/Plot1_1.svelte
@@ -0,0 +1,5 @@
+<enhanced:img
+    src="/static/images/posts/plot_1_1.png?w=800;400"
+    alt="Particle trajectory"
+    class="rounded-md shadow-lg"
+/>
---
+++ src/lib/images/Plot1_2.svelte
@@ -0,0 +1,5 @@
+<enhanced:img
+    src="/static/images/posts/plot_1_2.png?w=800;400"
+    alt="Example cubic spline"
+    class="rounded-md shadow-lg"
+/>
