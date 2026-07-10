64868ebe3206882bacda706778e7cc62c9dbf93f
Albert Johannessen <albertjohannessen@live.no>
2026-07-10 17:27:02 +0200

    adjusted post margins

--- src/routes/writing/[slug]/+page.svelte
+++ src/routes/writing/[slug]/+page.svelte
@@ -9,16 +9,16 @@
 
 <Meta {title} {description} {date} type="article" />
 
-<div class="mx-auto w-full max-w-4xl pt-5 pb-20">
+<div class="mx-auto w-full max-w-5xl pt-5 pb-20">
     <a
         href="/writing"
-        class="text-linklight dark:text-linkdark text-lg font-semibold underline"
+        class="text-linklight dark:text-linkdark text-lg font-semibold underline px-10"
         aria-label="Back to Writing"
     >
         &larr; Back
     </a>
 
-    <article class="prose dark:prose-invert pt-10 mx-auto w-full max-w-3xl">
+    <article class="prose dark:prose-invert pt-10 px-5 mx-auto w-full max-w-5xl">
         <h1 class="text-center">{title}</h1>
         <h2 class="text-center pb-10">{description}</h2>
 
