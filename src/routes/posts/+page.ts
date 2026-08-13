765c5548095d6989a354c548e53819033e5d359a
Albert Johannessen <albertjohannessen@live.no>
2026-08-14 01:19:50 +0200

    made it possible to push

--- src/routes/posts/+page.ts
+++ src/routes/posts/+page.ts
@@ -9,20 +9,27 @@ interface Post {
 const posts = import.meta.glob<Post>("$lib/posts/*.svx");
 
 export async function load() {
-    const entries = await Promise.all(
-        Object.entries(posts).map(async ([path, importer]) => {
-            const post = await importer();
-            const slug = path.split("/").at(-1)!.replace(".svx", "");
-            const id = Number(slug.split("_").at(0));
-            return {
-                slug: slug,
-                metadata: post.metadata,
-                id: id
-            };
-        })
-    );
+    const entries = (
+        await Promise.all(
+            Object.entries(posts).map(async ([path, importer]) => {
+                const post = await importer();
+                const slug = path.split("/").at(-1)!.replace(".svx", "");
+                const id = Number(slug.split("_").at(0));
 
-    entries.sort((a, b) => b.id - a.id); // Newest posts first
+                if (Number.isNaN(id)) {
+                    return null;
+                }
+
+                return {
+                    slug: slug,
+                    metadata: post.metadata,
+                    id: id
+                };
+            })
+        )
+    ).filter((entry) => { return entry !== null; });
+
+    entries.sort((a, b) => b.id - a.id);
 
     return { posts: entries };
 }
