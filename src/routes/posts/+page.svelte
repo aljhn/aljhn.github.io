0a1b37fab359f1e610808d3981975e2716f1b740
Albert Johannessen <albertjohannessen@live.no>
2026-08-14 01:03:54 +0200

    started setup for next post

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -13,14 +13,21 @@
         {#each data.posts as post (post.id)}
             <a
                 href={resolve(`/posts/${post.slug}`)}
-                class="bg-bglight-2 dark:bg-bgdark-2 darkModeFade block rounded-xl p-6 shadow-xl"
+                class="bg-bglight-2 dark:bg-bgdark-2 darkModeFade rounded-xl shadow-xl flex"
             >
-                <h2 class="text-textlight-1 dark:text-textdark-1 darkModeFade text-xl font-semibold">
-                    {post.metadata?.title ?? post.slug}
-                </h2>
-                {#if post.metadata?.description}
-                    <p class="text-textlight-2 dark:text-textdark-2 darkModeFade pt-1">{post.metadata.description}</p>
-                {/if}
+                <div class="bg-bglight-3 dark:bg-bgdark-3 darkModeFade rounded-xl shadow-xl flex items-center justify-center px-10">
+                    <p class="text-textlight-1 dark:text-textdark-1 darkModeFade text-xl font-semibold">{post.id}</p>
+                </div>
+                <div class="px-5 py-5">
+                    <h2 class="text-textlight-1 dark:text-textdark-1 darkModeFade text-xl font-semibold">
+                        {post.metadata?.title ?? post.slug}
+                    </h2>
+                    {#if post.metadata?.description}
+                        <p class="text-textlight-2 dark:text-textdark-2 darkModeFade pt-1">
+                            {post.metadata.description}
+                        </p>
+                    {/if}
+                </div>
             </a>
         {/each}
     </div>
